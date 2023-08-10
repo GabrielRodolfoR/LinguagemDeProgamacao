fac348bc16f34f30e890a4e9e250124d12214bf1
Gabriel Rodolfo Rabello <gabrielrodolforabello@gmail.com>
2023-08-10 16:24:57 -0300

    a

---
+++ Aula 1/Exemplo 10.py
@@ -0,0 +1,7 @@
+a = input("Digite seu nome: ")
+b = "Joaquim"
+print(f"Seja bem vindo {a:20}")
+print(f"Seja bem vindo {a:>20} ")
+print(f"Seja bem vindo {a:<20} e {b}")
+print(f"Seja bem vindo {a:^20} e {b}")
+print(f"Seja bem vindo {a:-^20}")
---
+++ Aula 1/Exemplo 11.py
@@ -0,0 +1,1 @@
+a = input("Digite seu nome: ")
