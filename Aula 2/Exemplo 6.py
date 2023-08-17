952ef586588d88abcc514e0ce970e890df2269ce
Gabriel Rodolfo Rabello <gabrielrodolforabello@gmail.com>
2023-08-17 14:49:34 -0300

    Update Exemplo 6.py

---
+++ Aula 2/Exemplo 6.py
@@ -0,0 +1,17 @@
+a = ["Gabriel", "Rodolfo", "Marcos", "Nunes", "Joao"]
+
+a.clear() #Limpa toda a lista
+print(a)
+
+a = ["Gabriel", "Rodolfo", "Marcos", "Nunes", "Joao"]
+
+c = a.copy() #Copia a lista para outra variável
+print(a)
+print(c)
+
+d = a + c #Juntar listas
+print(d)
+
+print(a.count("Gabriel")) #numeros de vezes que apareceu na lista
+print(len(a))
+
