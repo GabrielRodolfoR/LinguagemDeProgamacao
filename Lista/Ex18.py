797df5085787b6ea2fedde8a244b1f20314f9185
Gabriel Rodolfo Rabello <gabrielrodolforabello@gmail.com>
2023-08-24 15:30:01 -0300

    Update Ex18.py

--- Lista/Ex18.py
+++ Lista/Ex18.py
@@ -3,5 +3,9 @@ n2 = float(input("Digite a segunda nota: "))
 n3 = float(input("Digite a terceir nota: "))
 
 media = (n1+n2+n3)/2
+if media >=6:
+    est = "passou"
+else:
+    est = "reprovou"
 
-print(f"media: {media}")
+print(f"media: {media}, logo, {est}")
