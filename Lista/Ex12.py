cddb13f8ae4a9ca0015f8d76f14af3a2270a68c5
Gabriel Rodolfo Rabello <gabrielrodolforabello@gmail.com>
2023-08-24 14:34:43 -0300

    Update Ex12.py

---
+++ Lista/Ex12.py
@@ -0,0 +1,17 @@
+filmes = ["Alien", "Homem de Ferro", "Drácula", "Alien", "Super-Homem"]
+jogos = ["FNAF", "EU4", "CK3", "CK2", "EU4"]
+livros = ["BNHA", "One-Punch", "One-Punch", "Diario de um banana", "Island"]
+esporte = ["Futebol", "Voley", "Basquete", "Ping-Pong", "Futebol"]
+
+for i in filmes:
+    if filmes(i) in filmes:
+        print(filmes(i))
+for j in jogos:
+    if jogos(i) in jogos:
+        print(jogos(i))
+for k in livros:
+    if livros(i) in livros:
+        print(livros(i))
+for l in esporte:
+    if esporte(i) in esporte:
+        print(filmes(i))
